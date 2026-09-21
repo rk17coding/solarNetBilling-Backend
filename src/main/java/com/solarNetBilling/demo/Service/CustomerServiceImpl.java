b69e5a79141769800df1e68c3a47bd8baf21d85a
rk17coding <rahulstudy1711@gmail.com>
2026-09-21 17:24:34 +0530

    log4j2 configuration done

--- src/main/java/com/solarNetBilling/demo/Service/CustomerServiceImpl.java
+++ src/main/java/com/solarNetBilling/demo/Service/CustomerServiceImpl.java
@@ -2,30 +2,40 @@ package com.solarNetBilling.demo.Service;
 
 import com.solarNetBilling.demo.DAO.CustomerDAO;
 import com.solarNetBilling.demo.Model.Customer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CustomerServiceImpl {
+	
+	private static final Logger logger=LogManager.getLogger(CustomerServiceImpl.class);
 
     private final CustomerDAO CustomerDAO;
 
     public CustomerServiceImpl(CustomerDAO CustomerDAO) {
         this.CustomerDAO = CustomerDAO;
+        logger.info("Inside CustomerServiceImpl Constructor");
     }
 
     public List<Customer> getAllCustomers() {
+    	logger.info("Inside getAllCustomers()");
         return CustomerDAO.findAll();
     }
 
     public Customer getCustomerById(String id) {
+        logger.info("Inside getCustomerById(), id: ", id);
         return CustomerDAO.findById(id)
                 .orElseThrow(() -> new RuntimeException("Customer not found: " + id));
     }
 
     public String createCustomer(Customer Customer) {
+    	logger.info("Inside createCustomer()");
         int rows = CustomerDAO.save(Customer);
+        logger.info("No of rows effected:", rows);
         return rows > 0 ? "Customer created successfully" : "Creation failed";
     }
 
